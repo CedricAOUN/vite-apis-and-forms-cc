58d7918bd73a25d002094a2cb3f8f2f2da5726e1
CedricA <cedric.j.aoun@gmail.com>
2025-03-31 16:26:11 +0200

    act 8: added error handling and loading states

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import ProductCard from './ProductCard/ProductCard';
-import { Button, Container, Row } from 'react-bootstrap';
+import { Button, Container, Row, Spinner } from 'react-bootstrap';
 
 function App() {
   const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchProduct() {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data = await response.json();
-      setProductList(data);
+      setError(null); // Reset error
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+
+        if(!response.ok) {
+          throw new Error(`HTTP ERROR: ${response.status} - Message: ${response.statusText}`)
+        }
+
+        const data = await response.json();
+        setProductList(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchProduct();
   }, [])
@@ -19,28 +34,46 @@ function App() {
   // console.log(productList);
 
   async function handleAddProduct () {
-    const response = await fetch("https://fakestoreapi.com/products", {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "USB 128GB",
-        price: 49.99,
-        description: "USB with 128GB of storage",
-        image: "https://m.media-amazon.com/images/I/61T85CGI4ZL.jpg",
-        category: "electronics",
-      }),
-    });
-    const data = await response.json();
-    return alert(`Le produit avec l'id ${data.id} a été créé`);
-  };
+    setError(null);
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://fakestoreapi.com/products", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "USB 128GB",
+          price: 49.99,
+          description: "USB with 128GB of storage",
+          image: "https://m.media-amazon.com/images/I/61T85CGI4ZL.jpg",
+          category: "electronics",
+        }),
+      });
 
+      if(!response.ok) {
+        throw new Error(`HTTP ERROR: ${response.status} - Message: ${response.statusText}`)
+      }
+
+      const data = await response.json();
+      return alert(`Le produit avec l'id ${data.id} a été créé`);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <>
       <Container>
         <Button onClick={handleAddProduct}>Ajouter un produit</Button>
         <Row className='g-3 justify-content-center'>
+          {isLoading && (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )}
+          {error && (<p className='text-danger text-center'>{error.message}</p>)}
           {productList.map((product, index) => 
             <ProductCard 
               key={index}
@@ -48,7 +81,8 @@ function App() {
               title={product.title}
               imgURL={product.image} 
               desc={product.description} 
-              price={product.price} 
+              price={product.price}
+              onError={(err) => setError(err)}
             />
           )}
         </Row>
--- src/ProductCard/ProductCard.jsx
+++ src/ProductCard/ProductCard.jsx
@@ -1,47 +1,87 @@
-import React from 'react'
-import { Button, Card, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Card, Col, Spinner } from 'react-bootstrap'
 import './productCard.css'
 
-function ProductCard({ id, title, imgURL, desc, price }) {
+function ProductCard({ id, title, imgURL, desc, price, onError }) {
+  const [isCardLoading, setIsCardLoading] = useState(false);
 
   async function handleEditProduct() {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: 'Produit mis à jour',
-        price: 49.99,
-        description: 'Description mise à jour',
-        image: 'https://via.placeholder.com/150',
-        category: 'electronics',
-      }),
-    });
-    const data = await response.json();
-    return alert(`Le produit avec l'id ${data.id} a été modifié`);
+    onError(null);
+    setIsCardLoading(true);
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: 'Produit mis à jour',
+          price: 49.99,
+          description: 'Description mise à jour',
+          image: 'https://via.placeholder.com/150',
+          category: 'electronics',
+        }),
+      });
+
+      if(!response.ok) {
+        throw new Error(`HTTP Error: ${response.status} - Message: ${response.statusText}`)
+      }
+
+      const data = await response.json();
+      return alert(`Le produit avec l'id ${data.id} a été modifié`);
+    } catch(err) {
+      onError(err)
+    } finally {
+      setIsCardLoading(false);
+    }
   }
 
   async function handleEditPrice() {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        price: 54.99,
-      }),
-    });
-    const data = await response.json();
-    return alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
+    onError(null);
+    setIsCardLoading(true);
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          price: 54.99,
+        }),
+      });
+
+      if(!response.ok) {
+        throw new Error(`HTTP Error: ${response.status} - Message: ${response.statusText}`)
+      }
+
+      const data = await response.json();
+      return alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
+    } catch (err) {
+      onError(err)
+    } finally {
+      setIsCardLoading(false);
+    }
   }
 
   async function handleDeleteProduct() {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: 'DELETE',
-    });
-    const data = await response.json();
-    return alert(`Le produit avec l'id ${data.id} a été supprimé`);
+    onError(null);
+    setIsCardLoading(true);
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: 'DELETE',
+      });
+
+      if(!response.ok) {
+        throw new Error(`HTTP Error: ${response.status} - Message: ${response.statusText}`)
+      }
+
+      const data = await response.json();
+      return alert(`Le produit avec l'id ${data.id} a été supprimé`);
+    } catch (err) {
+      onError(err)
+    } finally {
+      setIsCardLoading(false);
+    }
   }
 
   return (
@@ -54,9 +94,19 @@ function ProductCard({ id, title, imgURL, desc, price }) {
             {desc}
           </Card.Text>
           <p>{price}€</p>
-          <Button onClick={handleEditProduct}>Modifier le produit complet</Button>
-          <Button onClick={handleEditPrice}>Modifier le prix du produit</Button>
-          <Button onClick={handleDeleteProduct} variant='danger'>Supprimer le produit</Button>
+          {isCardLoading 
+          ? (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )
+          : (
+          <>
+            <Button onClick={handleEditProduct}>Modifier le produit complet</Button>
+            <Button onClick={handleEditPrice}>Modifier le prix du produit</Button>
+            <Button onClick={handleDeleteProduct} variant='danger'>Supprimer le produit</Button>
+          </>
+          )}
         </Card.Body>
       </Card>
     </Col>
